d89f61077dac6af049ff434b0dcff18a2cb397ab
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-08-06 01:14:03 +0100

    llamad2 custom module

---
+++ llamad2/src/goodmorning.rs
@@ -0,0 +1,68 @@
+use serenity::all::{Context, CreateMessage, GenericChannelId, Message, UserId};
+use serenity::small_fixed_array::FixedString;
+use tokio::sync::RwLock;
+
+use crate::LLAMA_GUILD;
+
+const GOOD_MORNINGS: [&str; 8] = [
+    "good morning",
+    "gm",
+    "goodmorning",
+    "good mornin",
+    "mornin",
+    "morning",
+    "g'mornin",
+    "g morn",
+];
+
+pub trait GoodMorningCache: Send + Sync + 'static {
+    fn insert_author(
+        &mut self,
+        channel_id: GenericChannelId,
+        author: UserId,
+        content: FixedString<u16>,
+    ) -> Option<UserId>;
+}
+
+pub struct GoodMorning;
+
+impl GoodMorning {
+    pub async fn run<Data: GoodMorningCache>(ctx: &Context, message: &Message) {
+        let prev_author = {
+            let data = ctx.data::<RwLock<Data>>();
+            let mut data = data.write().await;
+            data.insert_author(
+                message.channel_id,
+                message.author.id,
+                message.content.clone(),
+            )
+        };
+
+        if message.guild_id.is_none_or(|guild| guild != LLAMA_GUILD)
+            || message.author.bot()
+            || !is_good_morning(&message.content)
+        {
+            return;
+        }
+
+        if prev_author.is_some_and(|last_author| last_author != message.author.id) {
+            message
+                .channel_id
+                .send_message(
+                    &ctx.http,
+                    CreateMessage::new()
+                        .content("Good Morning dear viewer <:GROG:1393906582298955776>"),
+                )
+                .await
+                .unwrap();
+        }
+    }
+}
+
+fn is_good_morning(content: &str) -> bool {
+    let trimmed_content = content.trim();
+
+    GOOD_MORNINGS
+        .iter()
+        .any(|gm_prefix| trimmed_content.starts_with(gm_prefix))
+}
---
+++ llamad2/src/lib.rs
@@ -0,0 +1,6 @@
+use serenity::all::GuildId;
+
+pub mod goodmorning;
+pub use goodmorning::{GoodMorning, GoodMorningCache};
+
+const LLAMA_GUILD: GuildId = GuildId::new(1133034263579734037);
