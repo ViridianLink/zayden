e00a30340116abb5f2db115cda7da0bb0642718f
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-08-16 00:35:18 +0100

    move pool to isolated function

--- bot/src/sqlx_lib.rs
+++ bot/src/sqlx_lib.rs
@@ -4,15 +4,15 @@ use sqlx::PgPool;
 use sqlx::postgres::PgPoolOptions;
 
 pub trait PostgresPool {
-    async fn new_pool() -> sqlx::Result<PgPool> {
-        PgPoolOptions::new()
-            .max_connections(10)
-            .min_connections(1)
-            .connect(&env::var("DATABASE_URL").unwrap())
-            .await
-    }
-
     fn pool(&self) -> &PgPool;
 }
 
 pub struct GuildTable;
+
+pub async fn new_pool() -> sqlx::Result<PgPool> {
+    PgPoolOptions::new()
+        .max_connections(10)
+        .min_connections(1)
+        .connect(&env::var("DATABASE_URL").unwrap())
+        .await
+}
