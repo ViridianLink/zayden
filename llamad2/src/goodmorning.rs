06ebdfc4c80ef234551d5326ae4bd754d9cbeb60
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-08-16 00:42:49 +0100

    fix goodmorning logic

--- llamad2/src/goodmorning.rs
+++ llamad2/src/goodmorning.rs
@@ -28,7 +28,11 @@ pub struct GoodMorning;
 
 impl GoodMorning {
     pub async fn run<Data: GoodMorningCache>(ctx: &Context, message: &Message) {
-        let prev_author = {
+        if message.guild_id.is_none_or(|guild| guild != LLAMA_GUILD) || message.author.bot() {
+            return;
+        }
+
+        let prev_msg = {
             let data = ctx.data::<RwLock<Data>>();
             let mut data = data.write().await;
             data.insert(
@@ -38,16 +42,11 @@ impl GoodMorning {
             )
         };
 
-        if message.guild_id.is_none_or(|guild| guild != LLAMA_GUILD)
-            || message.author.bot()
-            || !is_good_morning(&message.content)
+        if is_good_morning(&message.content)
+            && prev_msg.is_some_and(|(last_author, content)| {
+                last_author != message.author.id && is_good_morning(&content)
+            })
         {
-            return;
-        }
-
-        if prev_author.is_some_and(|(last_author, content)| {
-            last_author != message.author.id && is_good_morning(&content)
-        }) {
             message
                 .channel_id
                 .send_message(
