e53e6a22f2a0feada84944f5ac1f560dcdf0734d
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-08-26 04:48:18 +0100

    add locking to cron thread

--- bot/src/handler/ready.rs
+++ bot/src/handler/ready.rs
@@ -1,20 +1,37 @@
+use std::sync::atomic::Ordering;
+
 use serenity::all::{Context, OnlineStatus, Ready};
 use sqlx::PgPool;
+use tokio::sync::RwLock;
 
-use crate::Result;
 use crate::cron::start_cron_jobs;
 use crate::handler::Handler;
+use crate::{CtxData, Result, ZAYDEN_ID};
 
 impl Handler {
-    pub async fn ready(ctx: &Context, ready: &Ready, pool: &PgPool) -> Result<()> {
+    pub async fn ready(&self, ctx: &Context, ready: &Ready, pool: &PgPool) -> Result<()> {
         println!("{} is connected!", ready.user.name);
 
         ctx.set_presence(None, OnlineStatus::Online);
 
-        let ctx = ctx.clone();
-        let pool = pool.clone();
+        CtxData::ready(ctx, ready).await;
 
-        tokio::spawn(async move { start_cron_jobs(ctx, pool).await });
+        if !self.started_cron.load(Ordering::Relaxed) {
+            {
+                let data = ctx.data::<RwLock<CtxData>>();
+                let mut data = data.write().await;
+                if ready.application.id.get() == ZAYDEN_ID.get() {
+                    data.setup_static_cron();
+                }
+            }
+
+            let ctx = ctx.clone();
+            let pool = pool.clone();
+
+            tokio::spawn(async move { start_cron_jobs(ctx, pool).await });
+
+            self.started_cron.store(true, Ordering::Relaxed);
+        }
 
         Ok(())
     }
