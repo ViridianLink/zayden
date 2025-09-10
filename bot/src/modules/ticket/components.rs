6c8ca323a745355151bfbae0d48c2e30ad235ec8
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-09-10 16:22:05 +0100

    remove game dev ticket from zayden

--- bot/src/modules/ticket/components.rs
+++ bot/src/modules/ticket/components.rs
@@ -1,4 +1,4 @@
-use serenity::all::{ComponentInteraction, CreateInputText, Http, InputTextStyle};
+use serenity::all::{ComponentInteraction, Http};
 use ticket::TicketComponent;
 
 use crate::Result;
@@ -7,18 +7,7 @@ use super::Ticket;
 
 impl Ticket {
     pub async fn ticket_create(http: &Http, component: &ComponentInteraction) -> Result<()> {
-        let version =
-            CreateInputText::new(InputTextStyle::Short, "Version", "version").placeholder("1.0.0");
-
-        let additional = CreateInputText::new(
-            InputTextStyle::Paragraph,
-            "Additional Information",
-            "additional",
-        )
-        .placeholder("Please provide any additional information that may help us assist you.")
-        .required(false);
-
-        TicketComponent::ticket_create(http, component, [version, additional]).await?;
+        TicketComponent::ticket_create(http, component, []).await?;
 
         Ok(())
     }
