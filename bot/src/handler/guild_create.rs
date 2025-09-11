b2d4c9b366389d1551c810ac39701173163dcfd3
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-09-11 17:33:43 +0100

    move command registering to dedicated function

--- bot/src/handler/guild_create.rs
+++ bot/src/handler/guild_create.rs
@@ -1,13 +1,12 @@
-use serenity::all::{Context, DiscordJsonError, ErrorResponse, Guild, HttpError, JsonErrorCode};
+use serenity::all::{Context, Guild};
 use sqlx::{PgPool, Postgres};
 use tokio::sync::RwLock;
 use zayden_core::ApplicationCommand;
 
-use crate::ctx_data::CtxData;
-use crate::modules;
-use crate::modules::events::live::Live;
+use crate::modules::events::Live;
 use crate::modules::lfg::{GuildTable, PostTable};
-use crate::{BRADSTER_GUILD, Result};
+use crate::modules::misc::Update;
+use crate::{BRADSTER_GUILD, CtxData, Result, ZAYDEN_GUILD};
 
 use super::Handler;
 
@@ -15,56 +14,26 @@ impl Handler {
     pub async fn guild_create(ctx: &Context, guild: &Guild, pool: &PgPool) -> Result<()> {
         let data = ctx.data::<RwLock<CtxData>>();
 
-        let commands = modules::global_register(ctx);
-
-        let (_, _, commands_result) = tokio::join!(
+        let _ = tokio::join!(
             lfg::events::guild_create::<CtxData, Postgres, GuildTable, PostTable>(ctx, guild, pool),
             CtxData::guild_create(data, guild),
-            guild.id.set_commands(&ctx.http, &commands),
         );
-        match commands_result {
-            Ok(_) => {}
-            Err(serenity::Error::Http(HttpError::UnsuccessfulRequest(ErrorResponse {
-                error:
-                    DiscordJsonError {
-                        code: JsonErrorCode::InvalidFormBody,
-                        errors,
-                        ..
-                    },
-                ..
-            }))) if errors
-                .first()
-                .is_some_and(|e| e.code == "BASE_TYPE_BAD_LENGTH") =>
-            {
-                let index = errors
-                    .first()
-                    .unwrap()
-                    .path
-                    .split_once('.')
-                    .unwrap()
-                    .0
-                    .parse::<usize>()
-                    .unwrap();
 
-                let command = commands.get(index).unwrap();
-
-                eprintln!("{errors:?} - {command:?}")
+        match guild.id {
+            BRADSTER_GUILD => {
+                BRADSTER_GUILD
+                    .create_command(&ctx.http, Live::register(ctx).unwrap())
+                    .await?;
             }
-            Err(e) => {
-                eprintln!("Unhandled command error: {e:?}")
+            ZAYDEN_GUILD => {
+                ZAYDEN_GUILD
+                    .create_command(&ctx.http, Update::register(ctx).unwrap())
+                    .await?;
             }
+            _ => {}
         }
 
-        if guild.id == 1222360995700150443 {
-            println!("Registered Zayden Guild")
-        } else if guild.id == BRADSTER_GUILD {
-            guild
-                .id
-                .create_command(&ctx.http, Live::register(ctx).unwrap())
-                .await?;
-
-            println!("Registered Bradster Guild");
-        }
+        println!("Registered {}", guild.name);
 
         Ok(())
     }
