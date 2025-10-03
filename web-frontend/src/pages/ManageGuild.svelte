dfa1adf492bccb0ba46ae189ba0c887dd24b9b1d
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-10-03 18:31:00 +0100

    guilds page

---
+++ web-frontend/src/pages/ManageGuild.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { get } from "../lib/backend-types";
+    import { type Guild } from "../lib/discord-types";
+    import NavBar from "../lib/NavBar.svelte";
+
+    export let id;
+
+    const guildPromise = get<Guild>(`/manage/${id}`);
+</script>
+
+<svelte:head>
+    <title>Servers - Zayden</title>
+</svelte:head>
+
+<NavBar />
+
+<main class="dashboard">
+    {#await guildPromise then guild}
+        <pre>{JSON.stringify(guild, null, 2)}</pre>
+    {/await}
+</main>
