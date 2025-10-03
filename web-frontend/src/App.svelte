37fc621534f8dbdf677515293cd370bcb7cbe51d
ScarletSun <kacpi.doro@gmail.com>
2025-10-03 02:59:53 +0200

    logging in will now change the login button into a dashboard also centered the alt text on images for the user avatar and discords

--- web-frontend/src/App.svelte
+++ web-frontend/src/App.svelte
@@ -4,14 +4,14 @@
   import Dashboard from "./pages/Dashboard.svelte";
   import "./app.css";
   import Redirect from "./lib/Redirect.svelte";
-  import { BASE_URL, discordInviteUrl } from "./lib/variables";
+  import * as vars from "./lib/variables";
 </script>
 
 <Router>
   <div>
     <Route path="/"><Home /></Route>
     <Route path="/dashboard"><Dashboard /></Route>
-    <Route path="/invite"><Redirect to={discordInviteUrl} /></Route>
-    <Route path="/login"><Redirect to="{BASE_URL}/login" /></Route>
+    <Route path="/invite"><Redirect to={vars.discordInviteUrl} /></Route>
+    <Route path="/login"><Redirect to="{vars.BASE_URL}/login" /></Route>
   </div>
 </Router>
