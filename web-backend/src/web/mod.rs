e27ea6b03b7e323fff3f986cb6fd7435f38025cb
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-10-04 20:06:58 +0100

    add member and settings endpoint

--- web-backend/src/web/mod.rs
+++ web-backend/src/web/mod.rs
@@ -9,11 +9,13 @@ use routes_login::discord_auth_callback_handler;
 const AUTH_TOKEN: &str = "auth-token";
 
 use crate::AppState;
-use crate::web::routes_guild::{channels, guild};
+use crate::web::routes_guild::{channels, guild, settings, zayden};
 
 pub fn routes() -> Router<AppState> {
     Router::new()
         .route("/auth/callback", get(discord_auth_callback_handler))
         .route("/guild/{id}", get(guild))
         .route("/guild/{id}/channels", get(channels))
+        .route("/users/@me/guilds/{id}/member", get(zayden))
+        .route("/guild/{id}/settings", get(settings))
 }
--- web-backend/src/web/routes_guild.rs
+++ web-backend/src/web/routes_guild.rs
@@ -5,19 +5,17 @@ use axum::Json;
 use axum::extract::{Path, State};
 use serde::{Deserialize, Serialize};
 use twilight_http::Client;
+use twilight_http::request::guild;
 use twilight_model::channel::Channel;
-use twilight_model::guild::Guild;
+use twilight_model::guild::{Guild, Member};
 use twilight_model::id::Id;
 
-use crate::{AppState, Result};
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct GuildResponse(Guild);
+use crate::{AppState, CLIENT_ID, Result};
 
 pub async fn guild(
     Path(id): Path<String>,
     State(state): State<AppState>,
-) -> Result<Json<Option<GuildResponse>>> {
+) -> Result<Json<Option<Guild>>> {
     // TODO: Recieve & validate user auth-token
 
     let guild_id = Id::new(id.parse().unwrap());
@@ -25,7 +23,7 @@ pub async fn guild(
     match state.discord_client.guild(guild_id).with_counts(true).await {
         Ok(response) => {
             let guild = response.model().await.unwrap();
-            Ok(Json(Some(GuildResponse(guild))))
+            Ok(Json(Some(guild)))
         }
         Err(e) => Ok(Json(None)),
     }
@@ -44,4 +42,27 @@ pub async fn channels(
         }
         Err(e) => Ok(Json(None)),
     }
+}
+
+pub async fn zayden(
+    Path(guild_id): Path<String>,
+    State(state): State<AppState>,
+) -> Result<Json<Option<Member>>> {
+    let guild_id = Id::new(guild_id.parse().unwrap());
+
+    match state
+        .discord_client
+        .current_user_guild_member(guild_id)
+        .await
+    {
+        Ok(response) => {
+            let member = response.model().await.unwrap();
+            Ok(Json(Some(member)))
+        }
+        Err(e) => Ok(Json(None)),
+    }
+}
+
+pub async fn settings(Path(guild_id): Path<String>, State(state): State<AppState>) -> Result<()> {
+    todo!()
 }
