bdf97dd49dcf15c8bf96a8b130777ad1134aebe5
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-10-04 12:25:35 +0100

    add channels route

--- web-backend/src/web/mod.rs
+++ web-backend/src/web/mod.rs
@@ -9,10 +9,11 @@ use routes_login::discord_auth_callback_handler;
 const AUTH_TOKEN: &str = "auth-token";
 
 use crate::AppState;
-use crate::web::routes_guild::guild;
+use crate::web::routes_guild::{channels, guild};
 
 pub fn routes() -> Router<AppState> {
     Router::new()
         .route("/auth/callback", get(discord_auth_callback_handler))
-        .route("/manage/{id}", get(guild))
+        .route("/guild/{id}", get(guild))
+        .route("/guild/{id}/channels", get(channels))
 }
--- web-backend/src/web/routes_guild.rs
+++ web-backend/src/web/routes_guild.rs
@@ -2,30 +2,45 @@ use std::env;
 use std::time::Duration;
 
 use axum::Json;
-use axum::extract::Path;
+use axum::extract::{Path, State};
 use serde::{Deserialize, Serialize};
 use twilight_http::Client;
+use twilight_model::channel::Channel;
 use twilight_model::guild::Guild;
 use twilight_model::id::Id;
 
-use crate::Result;
+use crate::{AppState, Result};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GuildResponse(Guild);
 
-pub async fn guild(Path(id): Path<String>) -> Result<Json<Option<GuildResponse>>> {
-    // Recieve user auth-token
+pub async fn guild(
+    Path(id): Path<String>,
+    State(state): State<AppState>,
+) -> Result<Json<Option<GuildResponse>>> {
+    // TODO: Recieve & validate user auth-token
 
-    let client = Client::builder()
-        .token(env::var("DISCORD_TOKEN").unwrap())
-        .timeout(Duration::from_secs(20))
-        .build();
     let guild_id = Id::new(id.parse().unwrap());
 
-    match client.guild(guild_id).with_counts(true).await {
+    match state.discord_client.guild(guild_id).with_counts(true).await {
         Ok(response) => {
             let guild = response.model().await.unwrap();
             Ok(Json(Some(GuildResponse(guild))))
+        }
+        Err(e) => Ok(Json(None)),
+    }
+}
+
+pub async fn channels(
+    Path(id): Path<String>,
+    State(state): State<AppState>,
+) -> Result<Json<Option<Vec<Channel>>>> {
+    let guild_id = Id::new(id.parse().unwrap());
+
+    match state.discord_client.guild_channels(guild_id).await {
+        Ok(response) => {
+            let guild = response.model().await.unwrap();
+            Ok(Json(Some(guild)))
         }
         Err(e) => Ok(Json(None)),
     }
