641aee3d5bfb323987aea01822056f2ef0e89558
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-10-04 12:25:50 +0100

    styling fix

--- web-frontend/src/App.svelte
+++ web-frontend/src/App.svelte
@@ -6,14 +6,14 @@
     import Redirect from "./lib/Redirect.svelte";
     import ManageGuild from "./pages/ManageGuild.svelte";
     import { baseUrl } from "./lib/backend-types";
-    import { InviteUrl } from "./lib/discord-types";
+    import { INVITE_URL } from "./lib/discord-types/common";
 </script>
 
 <Router>
     <div>
         <Route path="/"><Home /></Route>
         <Route path="/dashboard"><Dashboard /></Route>
-        <Route path="/invite"><Redirect to={InviteUrl} /></Route>
+        <Route path="/invite"><Redirect to={INVITE_URL} /></Route>
         <Route path="/login"><Redirect to="{baseUrl}/login" /></Route>
         <Route path="/manage/:id" let:params
             ><ManageGuild id={params.id} /></Route
