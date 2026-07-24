2919e4c3678530052473f6546b3db0162f6e4467
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2026-07-24 18:12:57 +0100

    feat(gambling): add new SQL query for user gambling data retrieval

--- bot-modules/gambling/sql/LottoManager/rows.sql
+++ bot-modules/gambling/sql/LottoManager/rows.sql
@@ -4,7 +4,7 @@ SELECT
     i.quantity AS quantity
 FROM
     gambling g
-LEFT JOIN gambling_inventory
-    i ON g.user_id = i.user_id
-AND i.item_id = $1
-
+    LEFT JOIN gambling_inventory i ON g.user_id = i.user_id
+        AND i.item_id = $1
+WHERE
+    i.quantity > 0
