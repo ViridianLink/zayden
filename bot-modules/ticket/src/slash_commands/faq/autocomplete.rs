0dce1ff48de817d1fc8b21cdf738d79ecfd37db6
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2026-09-17 21:55:25 +0100

    Remove unused subcommand parser and simplify FAQ autocomplete query extraction

--- bot-modules/ticket/src/slash_commands/faq/autocomplete.rs
+++ bot-modules/ticket/src/slash_commands/faq/autocomplete.rs
@@ -6,10 +6,8 @@ use serenity::all::{
     CreateAutocompleteResponse,
     CreateInteractionResponse,
     Http,
-    ResolvedValue,
 };
 use zayden_app::state::AppState;
-use zayden_core::parse_subcommand;
 
 use crate::faq::index::choice::ask;
 use crate::faq::{FaqContext, WikiIndex};
@@ -26,7 +24,8 @@ impl Ticket {
             return respond(http, interaction, Vec::new()).await;
         };
 
-        let query = focused(interaction).unwrap_or_default();
+        let query =
+            interaction.data.autocomplete().map_or("", |option| option.value);
 
         // Autocomplete is the wrong place to report an unconfigured FAQ, so an
         // unusable config just means no suggestions.
@@ -40,26 +39,6 @@ impl Ticket {
 
         respond(http, interaction, choices).await
     }
-}
-
-fn focused(interaction: &CommandInteraction) -> Option<&str> {
-    let (_name, options) = parse_subcommand(interaction.data.options()).ok()?;
-
-    options.iter().find_map(|option| match option.value {
-        ResolvedValue::Autocomplete { value, .. } => Some(value),
-        ResolvedValue::Boolean(_)
-        | ResolvedValue::Integer(_)
-        | ResolvedValue::Number(_)
-        | ResolvedValue::String(_)
-        | ResolvedValue::SubCommand(_)
-        | ResolvedValue::SubCommandGroup(_)
-        | ResolvedValue::Attachment(_)
-        | ResolvedValue::Channel(_)
-        | ResolvedValue::Role(_)
-        | ResolvedValue::User(..)
-        | ResolvedValue::Unresolved(_)
-        | _ => None,
-    })
 }
 
 async fn respond(
